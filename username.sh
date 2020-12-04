f2cf36cc5b0ac77543ecccca39e7cca4aa75db96
Noah Henderson <nhenderson@chapman.edu>
2020-12-03 16:53:48 -1000

    Adds username.sh

--- username.sh
+++ username.sh
@@ -1,12 +1,13 @@
 #! /bin/bash
 # username.sh
 # Noah Henderson
+echo "Your username must be between three and tweleve lowercase letters, digits, and underscores. It must start with a lowercase letter."
 echo "Enter a Username: "
 read -r NAME
-while echo "$NAME" | grep -E -v "^[a-z][0-9]{5}$" > /dev/null 2>&1
+while echo "$NAME" | grep -E -v "^[a-z][0-9a-z_]{2,11}$" > /dev/null 2>&1
 do
-	echo "You must enter a valid ZIP code - five digits only!"
-	echo "Enter a five-digit ZIP code: "
+	echo "You must enter a valid username - between three and twelve lowercase letters, digits, and underscores only! Remember to start with a lowercase letter!"
+	echo "Enter a Username:"
 	read -r NAME
 done
 echo "Thank you"
